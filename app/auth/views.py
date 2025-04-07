799782f78780c45bd37aa632f5781de7e41e2007
ahsanmughal757 <mirzajack1995@gmail.com>
2025-04-07 17:48:54 +0500

    Working on Authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework.views import APIView
+from rest_framework import permissions, viewsets, status
 
+from rest_framework.response import Response
 from app.auth.serializers import GroupSerializer, UserSerializer
+
+from django.contrib.auth import authenticate
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +24,31 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    
+class Login(APIView):
+    
+    def get(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if not username or not password:
+            return Response({
+                "detail": "Please provide username and password."
+                }, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = authenticate(username=username, password=password)
+        
+        if not user:
+            return Response({
+                "detail": "Invalid credentials."
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({
+            "message": "Login successful.",
+        }, status=status.HTTP_200_OK)
+        
+        pass
+    
+    def post():
+        pass
--- app/settings.py
+++ app/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +78,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'app.wsgi.application'
+
+# Allow requests from your Next.js app
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+# If you need to allow credentials (cookies, auth headers)
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
